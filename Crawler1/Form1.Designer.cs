a419a68a63a9bc9ac8af4cf7bf4fbec37ab681bc
atfkaka <atfkaka@163.com>
2017-01-12 18:02:39 +0800

    获得网页源代码

--- Crawler1/Form1.Designer.cs
+++ Crawler1/Form1.Designer.cs
@@ -29,9 +29,14 @@
         private void InitializeComponent()
         {
             this.btn1 = new System.Windows.Forms.Button();
-            this.tb1 = new System.Windows.Forms.TextBox();
-            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.tbSource = new System.Windows.Forms.TextBox();
             this.wb1 = new System.Windows.Forms.WebBrowser();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn1
@@ -45,49 +50,77 @@
             this.btn1.UseVisualStyleBackColor = true;
             this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
-            // tb1
+            // tbUrl
             // 
-            this.tb1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tbUrl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tb1.Location = new System.Drawing.Point(12, 11);
-            this.tb1.Name = "tb1";
-            this.tb1.Size = new System.Drawing.Size(759, 25);
-            this.tb1.TabIndex = 1;
+            this.tbUrl.Location = new System.Drawing.Point(12, 11);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(759, 25);
+            this.tbUrl.TabIndex = 1;
+            this.tbUrl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbUrl_KeyDown);
             // 
-            // tb2
+            // tbSource
             // 
-            this.tb2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tbSource.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tb2.Location = new System.Drawing.Point(12, 303);
-            this.tb2.Multiline = true;
-            this.tb2.Name = "tb2";
-            this.tb2.Size = new System.Drawing.Size(820, 138);
-            this.tb2.TabIndex = 2;
+            this.tbSource.Location = new System.Drawing.Point(3, 3);
+            this.tbSource.Multiline = true;
+            this.tbSource.Name = "tbSource";
+            this.tbSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbSource.Size = new System.Drawing.Size(1490, 285);
+            this.tbSource.TabIndex = 2;
             // 
             // wb1
             // 
             this.wb1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.wb1.Location = new System.Drawing.Point(12, 42);
+            this.wb1.Location = new System.Drawing.Point(-69, -8);
             this.wb1.MinimumSize = new System.Drawing.Size(20, 20);
             this.wb1.Name = "wb1";
-            this.wb1.Size = new System.Drawing.Size(820, 255);
+            this.wb1.Size = new System.Drawing.Size(1490, 402);
             this.wb1.TabIndex = 4;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 42);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.wb1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tbSource);
+            this.splitContainer1.Size = new System.Drawing.Size(820, 394);
+            this.splitContainer1.SplitterDistance = 197;
+            this.splitContainer1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 437);
-            this.Controls.Add(this.wb1);
-            this.Controls.Add(this.tb2);
-            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.tbUrl);
             this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "网页解析";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,9 +129,10 @@
         #endregion
 
         private System.Windows.Forms.Button btn1;
-        private System.Windows.Forms.TextBox tb1;
-        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.TextBox tbSource;
         private System.Windows.Forms.WebBrowser wb1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- Crawler1/Form1.cs
+++ Crawler1/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Crawler1
 {
@@ -19,7 +23,75 @@ namespace Crawler1
 
         private void btn1_Click(object sender, EventArgs e)
         {//Q
-            //1
+            string url = tbUrl.Text;
+            wb1.Navigate(url);
+            tbSource.Text = getHtml(url, null);
+            ;
+        }
+
+
+        //url是要访问的网站地址，charSet是目标网页的编码，如果传入的是null或者""，那就自动分析网页的编码
+        private string getHtml(string url, string charSet)
+        {
+            //创建WebClient实例myWebClient
+            WebClient myWebClient = new WebClient();
+            // 需要注意的：
+            //有的网页可能下不下来，有种种原因比如需要cookie,编码问题等等
+            //这是就要具体问题具体分析比如在头部加入cookie
+            // webclient.Headers.Add("Cookie", cookie);
+            //这样可能需要一些重载方法。根据需要写就可以了
+
+            //获取或设置用于对向 Internet 资源的请求进行身份验证的网络凭据。
+            myWebClient.Credentials = CredentialCache.DefaultCredentials;
+            //如果服务器要验证用户名,密码
+            //NetworkCredential mycred = new NetworkCredential(struser, strpassword);
+            //myWebClient.Credentials = mycred;
+            //从资源下载数据并返回字节数组。（加@是因为网址中间有"/"符号）
+            byte[] myDataBuffer = myWebClient.DownloadData(url);
+            string strWebData = Encoding.Default.GetString(myDataBuffer);
+
+            //获取网页字符编码描述信息
+            Match charSetMatch = Regex.Match(strWebData, "<meta([^<]*)charset=([^<]*)\"", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            string webCharSet = charSetMatch.Groups[2].Value;
+            if (webCharSet != null || webCharSet != "")
+                webCharSet = webCharSet.ToUpper();
+            if (webCharSet.IndexOf("UTF-8")>0)
+            {
+                webCharSet = "UTF-8";
+            }
+            else if (webCharSet.IndexOf("GB2312") > 0)
+            {
+                webCharSet = "GB2312";
+            }
+            else if (webCharSet.IndexOf("GBK") > 0)
+            {
+                webCharSet = "GBK";
+            }
+            //            if (charSet != null && charSet != "" && Encoding.GetEncoding(charSet) != Encoding.Default)
+            if (webCharSet != null && webCharSet != "" )
+            {
+                Encoding enc = Encoding.GetEncoding(webCharSet);
+                strWebData = enc.GetString(myDataBuffer);
+            }
+                return strWebData;
+        }        
+
+        private void tbUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)//判断回车键  
+            {
+                this.btn1_Click(sender, e);//触发按钮事件 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            tbUrl.Focus();
         }
     }
 }
